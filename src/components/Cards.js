8b1a99c4616f3a4996a5bc8afad2d72b88796906
SatyamDutt <satyamdutt46@gmail.com>
2024-12-03 17:28:24 +0530

    03.12.2024

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -27,21 +27,21 @@ const Cards = () => {
         </div>
         <div className='cards'>
             <div className="card">
-                <img src="../smartwatch.png"></img>
+                <img src="../camera.png"></img>
                 <h2>₹ 1599</h2>
                 <h3>Black Smart Watch </h3>
                 <p>Elevate your daily routine with our state-of-the-art smartwatch. Combining sleek design and cutting-edge technology, </p>
                 <button className='cardButtons'>Buy Now</button>
             </div>
             <div className="card">
-                <img src="../headset.png"></img>
+                <img src="../speaker.png"></img>
                 <h2>₹ 1599</h2>
                 <h3>Black Smart Watch </h3>
                 <p>Elevate your daily routine with our state-of-the-art smartwatch. Combining sleek design and cutting-edge technology, </p>
                 <button className='cardButtons'>Buy Now</button>
             </div>
             <div className="card">
-                <img src="../redmiphone.png"></img>
+                <img src="../headphone.png"></img>
                 <h2>₹ 1599</h2>
                 <h3>Black Smart Watch </h3>
                 <p>Elevate your daily routine with our state-of-the-art smartwatch. Combining sleek design and cutting-edge technology, </p>
@@ -50,7 +50,7 @@ const Cards = () => {
         </div>
         <div>
             <img src="../othercontent.png"></img>
-            <img src="../othercontent.png"></img>
+            <img src="../othercontent2.png"></img>
         </div>
         </div>
     )
