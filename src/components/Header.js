0408dfcb8f31382ab9a00b358472c2cdbec2b316
SatyamDutt <satyamdutt46@gmail.com>
2025-11-29 17:28:32 +0530

    backgorund issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import './Header.css';
 const Header = () => {
     return (
-        <div className="heading">
+        <div className="heading bg-cover bg-center"
+        style={{backgroundImage: "url('/slider1.png')"}}
+        >
             <div className="title">
             <h1>Empowering the Next Generation<br></br> with Cutting-Edge Electronics</h1>
             <p>-Redefining Innovation for a Smarter Future!</p>
@@ -11,3 +13,27 @@ const Header = () => {
 }
 
 export default Header;
+
+//NEW
+// import './Header.css';
+
+
+// const Header = () => {
+//     return (
+//         <div
+//             className="heading bg-cover bg-center"
+//             style={{ backgroundImage: "url('/slider1.png')" }}
+//         >
+//             <div className="title">
+//                 <h1>
+//                     Empowering the Next Generation<br/>
+//                     with Cutting-Edge Electronics
+//                 </h1>
+//                 <p>- Redefining Innovation for a Smarter Future!</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// export default Header;
